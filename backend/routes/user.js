1e682fcb2ef06613d8b9b20d3218f091a9df3c50
sept1st2c <sept1st2c@gmail.com>
2024-06-11 06:47:10 +0530

    setting up user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,101 @@
 const express = require("express");
 const router = express.Router();
+const zod = require("zod");
+const jwt = require("jsonwebtoken");
+const { User } = require("../db");
+const JWT_SECRET = require("../config");
 
+//
+//
+//
+// sign up
+//
+//
+
+const signupSchema = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+  firstname: zod.string(),
+  lastname: zod.string(),
+});
+
+router.post("/signup", async (req, res) => {
+  const body = req.body;
+  const { success } = signupSchema.safeParse(req.body);
+
+  if (!success) {
+    return res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
+
+  const existinguser = await User.findOne({
+    username: body.username,
+  });
+
+  if (existinguser) {
+    return res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
+
+  const user = await User.create(req.body);
+  const userId = user._id;
+
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    message: "User created successfully",
+    token: token,
+  });
+});
+
+//
+//
+//
+// sign in
+//
+//
+
+const signinSchema = zod.object({
+  username: zod.string.email(),
+  password: zod.string(),
+});
+
+router.post("/signin", async (req, res) => {
+  const body = req.body;
+  const { success } = signinSchema.safeParse(body);
+
+  if (!success) {
+    return res.status(411).json({
+      message: "Error while logging in",
+    });
+  }
+
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      JWT_SECRET
+    );
+    return res.json({
+      token: token,
+    });
+  }
+
+  res.status(411).json({
+    message: "Error while logging in",
+  });
+});
 module.exports = router;
