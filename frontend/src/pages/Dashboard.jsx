48f10615d802606aef1884375215150700a7a7c5
sept1st2c <sept1st2c@gmail.com>
2024-06-12 09:55:27 +0530

    frontend structure completed

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,15 @@
+import { Appbar } from "../components/AppBar";
+import { Balance } from "../components/Balance";
+import { UsersComponent } from "../components/UsersComponent";
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Appbar />
+      <div className="m-8">
+        <Balance value={"10,000"} />
+        <UsersComponent />
+      </div>
+    </div>
+  );
 };
