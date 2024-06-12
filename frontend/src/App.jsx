6dbc0124451aaaa70db8f1cf0688b66a160fd9be
sept1st2c <sept1st2c@gmail.com>
2024-06-12 06:06:24 +0530

    added pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import { Dashboard } from "./pages/Dashboard";
+import { BrowserRouter } from "react-router-dom/dist/react-router-dom.development";
+import { Route } from "react-router-dom/dist/react-router-dom.development";
+import { Routes } from "react-router-dom/dist/react-router-dom.development";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Send } from "./pages/Send";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/send" element={<Send />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Send.jsx
@@ -0,0 +1,3 @@
+export const Send = () => {
+  return <div>Send</div>;
+};
